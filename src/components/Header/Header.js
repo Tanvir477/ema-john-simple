d5ef909ee2911f5932dff6b776e027499a4f7e8a
tanvir477 <tanvirhassan477@gmail.com>
2023-03-16 23:52:40 +0600

    cart Started

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,6 @@ const Header = () => {
                 <a href="/shop">Shop</a>
                 <a href="/review">Order Review</a>
                 <a href="/manage">Manage Inventory</a>
-                
             </nav>
 
         </div>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import'./Product.css'
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faShoppingCart} from "@fortawesome/free-solid-svg-icons"
+import "./Product.css";
 
 const Product = (props) => {
-    console.log(props.product);
-    const {img, name, seller, price, stock} = props.product;
+    // console.log(props);
+    const { img, name, seller, price, stock } = props.product;
     return (
-        <div className='product'>
+        <div className="product">
             <div>
-                <img src={img} alt='' />
+                <img src={img} alt="" />
             </div>
             <div>
-                <h4 className='product-name'>{name}</h4>
-                <br/>
-                <p><small>by:{seller}</small></p>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p>
+                    <small>by:{seller}</small>
+                </p>
                 <p>${price}</p>
-                
-                <p><small>only {stock} left in stock - order soon</small></p>
-            </div>
 
-            
+                <p>
+                    <small>only {stock} left in stock - order soon</small>
+                </p>
+                <button 
+                    className="main-button"
+                    onClick={()=>props.handleAddProduct(props.product)}
+                    >
+                        <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+            </div>
         </div>
     );
 };
